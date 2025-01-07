0fd3bd9771a659424383d2e347b08ce04ea6f4af
William Hermanrud <williamhermanrud@hotmail.com>
2025-01-07 12:05:22 +0100

    fix in revalitation key

--- src/app/api/revalidation/route.ts
+++ src/app/api/revalidation/route.ts
@@ -3,7 +3,7 @@ import { revalidateTag } from "next/cache";
 
 export async function POST(request: Request) {
   const requestHeaders = new Headers(request.headers);
-  const secret = requestHeaders.get("x-vercel-reval-key");
+  const secret = requestHeaders.get("x-vercel-revel-key");
 
   if (secret !== process.env.CONTENTFUL_REVALIDATE_SECRET) {
     return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
